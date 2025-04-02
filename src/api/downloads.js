c4d5dbe21f3e9a65bbfa8f2e51d4f521cc56dc93
melpi-eg <melpi@eg.dk>
2025-04-02 15:29:28 +0530

    added the env variable for the backend url

--- src/api/downloads.js
+++ src/api/downloads.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://eg-icons-backend-aaad382084ea.herokuapp.com",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
 });
 
 /**
